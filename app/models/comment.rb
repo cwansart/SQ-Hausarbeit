c24c8daecf51c67d78829e6d218b417573794d8c
Christian Wansart <cwansart@users.noreply.github.com>
2016-08-01 11:57:06 +0200

    Added association validation to post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post
 
+  validates_associated :post
   validates :author, presence: true
   validates :title, presence: true
   validates :body, presence: true
