dbeaaae8bc972ce56a1a50f1faee0f0b48b1721c
Christian Wansart <cwansart@users.noreply.github.com>
2016-08-04 23:36:50 +0200

    Some tests

--- test/integration/posts_index_test.rb
+++ test/integration/posts_index_test.rb
@@ -1,8 +1,19 @@
 require 'test_helper'
 
 class PostsIndexTest < ActionDispatch::IntegrationTest
+  setup do
+    @comment = comments(:one)
+  end
+
   test "if there is a posts table" do
     get "/posts"
     assert_select "table"
+  end
+
+  test "if comment is displayed" do
+    get comments_url
+    assert_select "td", @comment.author
+    assert_select "td", @comment.title
+    assert_select "td", @comment.body
   end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid with valid attributes" do
+    post = Post.new(title: 'Title', author: 'Autor', body: 'Body', published_at: DateTime.now)
+    assert post.save
+  end
+
+  test "is not valid without title" do
+    post = Post.new(author: 'Autor', body: 'Body', published_at: DateTime.now)
+    assert_not post.save
+  end
+
+  test "is not valid without author" do
+    post = Post.new(title: 'Title', body: 'Body', published_at: DateTime.now)
+    assert_not post.save
+  end
+
+  test "is not valid without body" do
+    post = Post.new(title: 'Title', author: 'Autor', published_at: DateTime.now)
+    assert_not post.save
+  end
+
+  test "is not valid without publish date" do
+    post = Post.new(title: 'Title', author: 'Autor', body: 'Body')
+    assert_not post.save
+  end
+
+  test "is not valid without attributes" do
+    post = Post.new
+    assert_not post.save
+  end
 end
