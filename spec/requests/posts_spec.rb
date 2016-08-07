23acad3a3b47de4c603fa921718a5beda09a9c59
Christian Wansart <cwansart@users.noreply.github.com>
2016-08-07 17:22:37 +0200

    Added post request specs

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,16 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
+  let(:valid_attributes) {
+    { title: 'Mein Titel', author: 'Karl', body: 'Lorem Ipsum', published_at: DateTime.now }
+  }
+
+  let(:new_attributes) {
+    { title: 'Mein Titel2', author: 'Karl2', body: 'Lorem Ipsum2', published_at: DateTime.now }
+  }
+
   describe "GET /posts" do
-    it "works! (now write some real specs)" do
-      get posts_path
-      expect(response).to have_http_status(200)
+    it "creates a post and redirects to show view" do
+      get new_post_url
+      expect(response).to render_template(:new)
+
+      post posts_url, params: { post: valid_attributes }
+      expect(response).to redirect_to(Post.last)
+      follow_redirect!
+
+      expect(response).to render_template(:show)
+      expect(response.body).to include("Post was successfully created.")
+    end
+
+    it "deletes a post and redirects posts index" do
+      post = Post.create! valid_attributes
+
+      get posts_url
+      expect(response).to render_template(:index)
+
+      delete post_url(post)
+      expect(response).to redirect_to(posts_url)
+      follow_redirect!
+
+      expect(response).to render_template(:index)
+      expect(response.body).to include("Post was successfully destroyed.")
+    end
+
+    it "updates a post and redirect to show" do
+      post = Post.create! valid_attributes
+
+      get edit_post_url(post)
+      expect(response).to render_template(:edit)
+
+      patch post_url(post), params: { post: new_attributes }
+      expect(response).to redirect_to(post)
+      follow_redirect!
+
+      expect(response).to render_template(:show)
+      expect(response.body).to include("Post was successfully updated.")
     end
   end
-  
-#  describe "POST /posts" do
-#    it 'should save the post and show a success message' do
-#
-#    end
-#  end
 end
