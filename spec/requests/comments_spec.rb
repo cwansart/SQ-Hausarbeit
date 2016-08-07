e9a122a711970b181fe58e10f78470df722103e8
Christian Wansart <cwansart@users.noreply.github.com>
2016-08-07 17:12:46 +0200

    Added comments request spec

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -2,14 +2,56 @@ require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
   before(:each) do
-    @post = Post.create!(title: 'Titel', author: 'Author', body: 'blubb', published_at: DateTime.now)
-    @comment = Comment.create!(post_id: @post.id, title: 'Mein Titel', author: 'Karl', body: 'Lorem Ipsum', published_at: DateTime.now)
+    @post = Post.create! title: 'Titel', author: 'Author', body: 'blubb', published_at: DateTime.now
   end
 
+  let(:valid_attributes) {
+    { post_id: @post.id, title: 'Mein Titel', author: 'Karl', body: 'Lorem Ipsum', published_at: DateTime.now }
+  }
+
+  let(:new_attributes) {
+    { post_id: @post.id, title: 'Mein Titel2', author: 'Karl2', body: 'Lorem Ipsum2', published_at: DateTime.now }
+  }
+
   describe "GET /comments" do
-    it "works! (now write some real specs)" do
-      get comments_path
-      expect(response).to have_http_status(200)
+    it "creates a comment and redirects to it's corresponding post" do
+      get post_url(@post)
+      expect(response).to render_template(:show)
+
+      post comments_url, params: { comment: valid_attributes }
+      expect(response).to redirect_to(Comment.last.post)
+      follow_redirect!
+
+      expect(response).to render_template(:show)
+      expect(response.body).to include("Comment was successfully created.")
+    end
+
+    it "deletes a comment and redirects to it's post" do
+      comment = Comment.create! valid_attributes
+
+      get post_url(@post)
+      expect(response).to render_template(:show)
+
+      delete comment_url(comment)
+      expect(response).to redirect_to(@post)
+      follow_redirect!
+
+      expect(response).to render_template(:show)
+      expect(response.body).to include("Comment was successfully destroyed.")
+    end
+
+    it "updates a comment and redirect to it's post" do
+      comment = Comment.create! valid_attributes
+
+      get edit_comment_url(comment)
+      expect(response).to render_template(:edit)
+
+      patch comment_url(comment), params: { comment: new_attributes }
+      expect(response).to redirect_to(@post)
+      follow_redirect!
+
+      expect(response).to render_template(:show)
+      expect(response.body).to include("Comment was successfully updated.")
     end
   end
 end
