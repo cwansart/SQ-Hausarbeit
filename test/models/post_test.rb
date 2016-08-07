66bb4926a457b61c885e5c2aadfc42794a294996
Christian Wansart <cwansart@users.noreply.github.com>
2016-08-07 15:21:50 +0200

    Added post model spec

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save post without author, body and title" do
+    post = Post.new
+    assert_not post.save
+  end
+
+  test "should not save without author" do
+    post = Post.new body: "Lorem Ipsum", title: "My Title", published_at: DateTime.now
+    assert_not post.save
+  end
+
+  test "should not save without body" do
+    post = Post.new author: "Karl", title: "My Title", published_at: DateTime.now
+    assert_not post.save
+  end
+
+  test "should not save without title" do
+    post = Post.new author: "Karl", body: "Lorem Ipsum", published_at: DateTime.now
+    assert_not post.save
+  end
+
+  test "should not save without publish date" do
+    post = Post.new author: "Karl", body: "Lorem Ipsum", title: "My Title"
+    assert_not post.save
+  end
+
+  test "should save comment" do
+    post = Post.new author: "Karl", title: "Hello World", body: "Lorem Ipsum", published_at: DateTime.now
+    assert post.save
+  end
 end
