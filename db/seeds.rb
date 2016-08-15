caca6db69e0d184889841d9a6ff6a9ad71814025
Christian Wansart <cwansart@users.noreply.github.com>
2016-08-15 14:52:34 +0200

    Added seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+post  = Post.create! title: 'Herzlich Willkommen', author: 'Admin', body: 'Herzlich Willkommen auf unserem Blog. Hier möchten wir verschiedene Test-Frameworks für Rails vorstellen und vergleichen.', published_at: DateTime.now
+post2 = Post.create! title: 'Test-Eintrag', author: 'Admin', body: 'Das ist bloß ein kleines Beispiel. Das könnt ihr gerne ignorieren. :-)', published_at: DateTime.now
+
+Comment.create! post_id: post.id, title: 'Freut mich!', author: 'David', body: 'Ein neues Blog zu Tests? Ich freue mich schon! :)', published_at: DateTime.now
+Comment.create! post_id: post.id, title: 'Sehr schön!', author: 'Sarah', body: 'Da kann ich David nur beipflichten! Ich freue mich auch schon sehr auf die Artikel!', published_at: DateTime.now
